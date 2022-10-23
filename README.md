9b8d0f544a9a261274755691d0c865a96d2d8f43
Pavlo Pashchevskyi <googalltooth@gmail.com>
2022-10-23 22:10:49 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is task#15 in JavaPro group of Hillel computer school.
+
+This task is about streams in Java.
---
+++ src/ua/ithillel/lms/Main.java
@@ -0,0 +1,8 @@
+package ua.ithillel.lms;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
