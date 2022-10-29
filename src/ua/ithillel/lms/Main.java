18865f7b51d4dc1a5ec0f175be1295aab876d4ce
Pavlo Pashchevskyi <googalltooth@gmail.com>
2022-10-29 14:45:41 +0300

    google code style has been applied

--- src/ua/ithillel/lms/Main.java
+++ src/ua/ithillel/lms/Main.java
@@ -11,51 +11,51 @@ import ua.ithillel.lms.javapro.pavlopashchevskyi.task15.exception.ProductNotFoun
 
 public class Main {
 
-    public static void main(String[] args) {
-        List<Product> products = new ArrayList<>();
-        Product book1 = new Product("123-456-789", "Book", 51);
-        Product book2 = new Product("123-456-790", "Book", 75);
-        Product magazine3 = new Product("234-567-890", "Magazine", 251);
-        Product book4 = new Product("123-456-791", "Book", 300);
-        Product book5 = new Product("123-456-792", "Book", 74.9);
-        products.add(book1);
-        products.add(book2);
-        products.add(magazine3);
-        products.add(book4);
-        products.add(book5);
-        ProductManager pm = new ProductManager(products);
+  public static void main(String[] args) {
+    List<Product> products = new ArrayList<>();
+    Product book1 = new Product("123-456-789", "Book", 51);
+    Product book2 = new Product("123-456-790", "Book", 75);
+    Product magazine3 = new Product("234-567-890", "Magazine", 251);
+    Product book4 = new Product("123-456-791", "Book", 300);
+    Product book5 = new Product("123-456-792", "Book", 74.9);
+    products.add(book1);
+    products.add(book2);
+    products.add(magazine3);
+    products.add(book4);
+    products.add(book5);
+    ProductManager pm = new ProductManager(products);
 
-        System.out.println("Expensive books:"); // Task 1.2
-        List<Product> filteredProducts = pm.getExpansiveBooks();
-        System.out.println(ProductManager.output(filteredProducts));
+    System.out.println("Expensive books:"); // Task 1.2
+    List<Product> filteredProducts = pm.getExpansiveBooks();
+    System.out.println(ProductManager.output(filteredProducts));
 
-        System.out.println("Discounted books"); // Task 2.1
-        book1.setDiscountable(true);
-        book4.setDiscountable(true);
-        List<Product> discountedProducts = pm.applyDiscountToBooks();
-        System.out.println(ProductManager.output(discountedProducts));
+    System.out.println("Discounted books"); // Task 2.1
+    book1.setDiscountable(true);
+    book4.setDiscountable(true);
+    List<Product> discountedProducts = pm.applyDiscountToBooks();
+    System.out.println(ProductManager.output(discountedProducts));
 
-        System.out.println("The cheapest book:");// Task 3.2
-        try {
-            Product theCheapestBook = pm.getTheCheapestBook();
-            System.out.println(theCheapestBook);
-        } catch (ProductNotFoundException e) { // Task 3.3
-            System.out.println(e.getMessage());
-        }
-
-        System.out.println("The latest added products:");// Task 4.2
-        book1.setAdditionDate(LocalDateTime.of(2021, 9, 24, 15, 0));
-        book2.setAdditionDate(LocalDateTime.of(2022, 10, 24, 15, 0));
-        magazine3.setAdditionDate(LocalDateTime.of(2022, 9, 8, 1, 0));
-        List<Product> theLatestAddedProducts = pm.getTheLatestAddedProducts();
-        System.out.println(ProductManager.output(theLatestAddedProducts));
-
-        System.out.println("Value of current year cheap books: ");// Task 5.2
-        double sum = pm.calcCurrentYearCheapBooksTotalValue();
-        System.out.println(sum);
-
-        System.out.println("Products, grouped by category"); // Task 6.2
-        Map<String, List<Product>> groupedProducts = pm.groupByType();
-        System.out.println(groupedProducts);
+    System.out.println("The cheapest book:");// Task 3.2
+    try {
+      Product theCheapestBook = pm.getTheCheapestBook();
+      System.out.println(theCheapestBook);
+    } catch (ProductNotFoundException e) { // Task 3.3
+      System.out.println(e.getMessage());
     }
+
+    System.out.println("The latest added products:");// Task 4.2
+    book1.setAdditionDate(LocalDateTime.of(2021, 9, 24, 15, 0));
+    book2.setAdditionDate(LocalDateTime.of(2022, 10, 24, 15, 0));
+    magazine3.setAdditionDate(LocalDateTime.of(2022, 9, 8, 1, 0));
+    List<Product> theLatestAddedProducts = pm.getTheLatestAddedProducts();
+    System.out.println(ProductManager.output(theLatestAddedProducts));
+
+    System.out.println("Value of current year cheap books: ");// Task 5.2
+    double sum = pm.calcCurrentYearCheapBooksTotalValue();
+    System.out.println(sum);
+
+    System.out.println("Products, grouped by category"); // Task 6.2
+    Map<String, List<Product>> groupedProducts = pm.groupByType();
+    System.out.println(groupedProducts);
+  }
 }
--- src/ua/ithillel/lms/javapro/pavlopashchevskyi/task15/ProductManager.java
+++ src/ua/ithillel/lms/javapro/pavlopashchevskyi/task15/ProductManager.java
@@ -10,105 +10,107 @@ import ua.ithillel.lms.javapro.pavlopashchevskyi.task15.exception.ProductNotFoun
 
 public class ProductManager {
 
-    private final List<Product> products;
+  private final List<Product> products;
 
-    /**
-     * Creates ProductManager instance by list of Product objects
-     *
-     * @param products list of Product instances
-     */
-    public ProductManager(List<Product> products) {
-        this.products = products;
-    }
+  /**
+   * Creates ProductManager instance by list of Product objects
+   *
+   * @param products list of Product instances
+   */
+  public ProductManager(List<Product> products) {
+    this.products = products;
+  }
 
-    /**
-     * Gets Product instances, which type is "book" and price is greater than 250
-     *
-     * @return List<Product>
-     */
-    public List<Product> getExpansiveBooks() {
-        return products.stream()
-                .filter(p -> p.getPrice() > 250 && "Book".equalsIgnoreCase(p.getType()))
-                .collect(Collectors.toList());
-    }
+  /**
+   * Gets Product instances, which type is "book" and price is greater than 250
+   *
+   * @return List<Product>
+   */
+  public List<Product> getExpansiveBooks() {
+    return products.stream()
+        .filter(p -> p.getPrice() > 250 && "Book".equalsIgnoreCase(p.getType()))
+        .collect(Collectors.toList());
+  }
 
-    /**
-     * Gets Product instances, which type is "book" and they are discountable.
-     * Makes discount 10% and returns list after that.
-     *
-     * @return List<Product>
-     */
-    public List<Product> applyDiscountToBooks() {
-        return products.stream()
-                .filter(p -> p.isDiscountable() && "Book".equalsIgnoreCase(p.getType()))
-                .peek(p -> p.setPrice(p.getPrice() - p.getPrice() * 0.1))
-                .collect(Collectors.toList());
-    }
+  /**
+   * Gets Product instances, which type is "book" and they are discountable. Makes discount 10% and
+   * returns list after that.
+   *
+   * @return List<Product>
+   */
+  public List<Product> applyDiscountToBooks() {
+    return products.stream()
+        .filter(p -> p.isDiscountable() && "Book".equalsIgnoreCase(p.getType()))
+        .peek(p -> p.setPrice(p.getPrice() - p.getPrice() * 0.1))
+        .collect(Collectors.toList());
+  }
 
-    /**
-     * Gets Product instances, which type is "book" and price is minimal
-     *
-     * @return Product - an instance of the cheapest product.
-     * @throws ProductNotFoundException when list of Product instances is empty.
-     */
-    public Product getTheCheapestBook() throws ProductNotFoundException {
-        if (products.stream().noneMatch(p -> "Book".equalsIgnoreCase(p.getType()))) {
-            throw new ProductNotFoundException("Book");
-        }
-        return products.stream()
-                .filter(p -> "Book".equalsIgnoreCase(p.getType()))
-                .min(Comparator.comparing(Product::getPrice))
-                .orElseGet(() -> new Product("000-000-000", "", 0.00));
+  /**
+   * Gets Product instances, which type is "book" and price is minimal
+   *
+   * @return Product - an instance of the cheapest product.
+   * @throws ProductNotFoundException when list of Product instances is empty.
+   */
+  public Product getTheCheapestBook() throws ProductNotFoundException {
+    if (products.stream().noneMatch(p -> "Book".equalsIgnoreCase(p.getType()))) {
+      throw new ProductNotFoundException("Book");
     }
+    return products.stream()
+        .filter(p -> "Book".equalsIgnoreCase(p.getType()))
+        .min(Comparator.comparing(Product::getPrice))
+        .orElseGet(() -> new Product("000-000-000", "", 0.00));
+  }
 
-    /**
-     * Gets three the latest added Product instances
-     *
-     * @return List<Product>
-     */
-    public List<Product> getTheLatestAddedProducts() {
-        return products.stream()
-                .sorted(Comparator.comparing(Product::getAdditionDate).reversed())
-                .limit(3)
-                .collect(Collectors.toList());
-    }
+  /**
+   * Gets three the latest added Product instances
+   *
+   * @return List<Product>
+   */
+  public List<Product> getTheLatestAddedProducts() {
+    return products.stream()
+        .sorted(Comparator.comparing(Product::getAdditionDate).reversed())
+        .limit(3)
+        .collect(Collectors.toList());
+  }
 
-    /**
-     * Calculates total price of Product instances, which type is "book" and price is non-greater than 75.
-     *
-     * @return double
-     */
-    public double calcCurrentYearCheapBooksTotalValue() {
-        return products.stream()
-                .filter(p -> "Book".equalsIgnoreCase(p.getType()) && p.getPrice() <= 75)
-                .filter(p -> p.getAdditionDate().getYear() == LocalDateTime.now().getYear())
-                .mapToDouble(Product::getPrice).sum();
-    }
+  /**
+   * Calculates total price of Product instances, which type is "book" and price is non-greater than
+   * 75.
+   *
+   * @return double
+   */
+  public double calcCurrentYearCheapBooksTotalValue() {
+    return products.stream()
+        .filter(p -> "Book".equalsIgnoreCase(p.getType()) && p.getPrice() <= 75)
+        .filter(p -> p.getAdditionDate().getYear() == LocalDateTime.now().getYear())
+        .mapToDouble(Product::getPrice).sum();
+  }
 
-    /**
-     * Groups Product instances by type. Returns map, which key is type and value is list of Product instances.
-     *
-     * @return Map<String, List < Product>>
-     */
-    public Map<String, List<Product>> groupByType() {
-        return products.stream()
-                .collect(Collectors.groupingBy(Product::getType));
-    }
+  /**
+   * Groups Product instances by type. Returns map, which key is type and value is list of Product
+   * instances.
+   *
+   * @return Map<String, List < Product>>
+   */
+  public Map<String, List<Product>> groupByType() {
+    return products.stream()
+        .collect(Collectors.groupingBy(Product::getType));
+  }
 
-    /**
-     * Static method for output list of Product instances as String.
-     *
-     * @param products list of Product instances
-     * @return String
-     */
-    public static String output(List<Product> products) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < products.size(); i++) {
-            sb.append("Product#");
-            sb.append(i + 1);
-            sb.append(":\n");
-            sb.append(products.get(i));
-        }
-        return sb.toString();
+  /**
+   * Static method for output list of Product instances as String.
+   *
+   * @param products list of Product instances
+   * @return String
+   */
+  public static String output(List<Product> products) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < products.size(); i++) {
+      sb.append("Product#");
+      sb.append(i + 1);
+      sb.append(":\n");
+      sb.append(products.get(i));
     }
+    return sb.toString();
+  }
 }
