c5784921844e6da26ff7e515c1acf21cb77a424c
Pavlo Pashchevskyi <googalltooth@gmail.com>
2022-10-29 14:36:59 +0300

    javadoc has been added

--- src/ua/ithillel/lms/javapro/pavlopashchevskyi/task15/Product.java
+++ src/ua/ithillel/lms/javapro/pavlopashchevskyi/task15/Product.java
@@ -5,74 +5,86 @@ import java.util.Objects;
 
 public class Product {
 
-  private final String ID;
+    private final String ID;
 
-  private final String type;
-  private double price;
+    private final String type;
+    private double price;
 
-  private boolean isDiscountable;
+    private boolean isDiscountable;
 
-  private LocalDateTime additionDate;
+    private LocalDateTime additionDate;
 
-  public Product(String ID, String type, double price) {
-    this.ID = ID;
-    this.type = type;
-    this.price = price;
-    this.isDiscountable = false;
-    this.additionDate = LocalDateTime.now();
-  }
-
-  public String getType() {
-    return type;
-  }
-
-  public double getPrice() {
-    return price;
-  }
-
-  public void setPrice(double price) {
-    this.price = price;
-  }
-
-  public boolean isDiscountable() {
-    return isDiscountable;
-  }
-
-  public void setDiscountable(boolean discountable) {
-    isDiscountable = discountable;
-  }
-
-  public LocalDateTime getAdditionDate() {
-    return additionDate;
-  }
-
-  public void setAdditionDate(LocalDateTime additionDate) {
-    this.additionDate = additionDate;
-  }
-
-  public String toString() {
-    String str = "ID: " + this.ID + "\n";
-    str += "Type: " + this.type + "\nPrice: " + this.price + "\nIs discountable?: ";
-    str += (this.isDiscountable) ? "Yes" : "No";
-    str += "\nAddition date: " + this.additionDate + "\n\n";
-
-    return str;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
+    /**
+     * Creates the Product object by its ID, type ("Book", "Magazine" etc.) and initial price)
+     *
+     * @param ID    identifier of product
+     * @param type  type of product (book, magazine etc.)
+     * @param price initial price of product
+     */
+    public Product(String ID, String type, double price) {
+        this.ID = ID;
+        this.type = type;
+        this.price = price;
+        this.isDiscountable = false;
+        this.additionDate = LocalDateTime.now();
     }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Product product = (Product) o;
-    return ID.equals(product.ID) && type.equals(product.type);
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(ID, type);
-  }
+    public String getType() {
+        return type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    /**
+     * Checks if Product is discountable
+     *
+     * @return boolean
+     */
+    public boolean isDiscountable() {
+        return isDiscountable;
+    }
+
+    public void setDiscountable(boolean discountable) {
+        isDiscountable = discountable;
+    }
+
+    public LocalDateTime getAdditionDate() {
+        return additionDate;
+    }
+
+    public void setAdditionDate(LocalDateTime additionDate) {
+        this.additionDate = additionDate;
+    }
+
+    public String toString() {
+        String str = "ID: " + this.ID + "\n";
+        str += "Type: " + this.type + "\nPrice: " + this.price + "\nIs discountable?: ";
+        str += (this.isDiscountable) ? "Yes" : "No";
+        str += "\nAddition date: " + this.additionDate + "\n\n";
+
+        return str;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return ID.equals(product.ID) && type.equals(product.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ID, type);
+    }
 }
--- src/ua/ithillel/lms/javapro/pavlopashchevskyi/task15/ProductManager.java
+++ src/ua/ithillel/lms/javapro/pavlopashchevskyi/task15/ProductManager.java
@@ -5,66 +5,110 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
 import ua.ithillel.lms.javapro.pavlopashchevskyi.task15.exception.ProductNotFoundException;
 
 public class ProductManager {
 
-  private final List<Product> products;
+    private final List<Product> products;
 
-  public ProductManager(List<Product> products) {
-    this.products = products;
-  }
-
-  public List<Product> getExpansiveBooks() {
-    return products.stream()
-        .filter(p -> p.getPrice() > 250 && "Book".equalsIgnoreCase(p.getType()))
-        .collect(Collectors.toList());
-  }
-
-  public List<Product> applyDiscountToBooks() {
-    return products.stream()
-        .filter(p -> p.isDiscountable() && "Book".equalsIgnoreCase(p.getType()))
-        .peek(p -> p.setPrice(p.getPrice() - p.getPrice() * 0.1))
-        .collect(Collectors.toList());
-  }
-
-  public Product getTheCheapestBook() throws ProductNotFoundException {
-    if (products.stream().noneMatch(p -> "Book".equalsIgnoreCase(p.getType()))) {
-      throw new ProductNotFoundException("Book");
+    /**
+     * Creates ProductManager instance by list of Product objects
+     *
+     * @param products list of Product instances
+     */
+    public ProductManager(List<Product> products) {
+        this.products = products;
     }
-    return products.stream()
-        .filter(p -> "Book".equalsIgnoreCase(p.getType()))
-        .min(Comparator.comparing(Product::getPrice))
-        .orElseGet(() -> new Product("000-000-000", "", 0.00));
-  }
 
-  public List<Product> getTheLatestAddedProducts() {
-    return products.stream()
-        .sorted(Comparator.comparing(Product::getAdditionDate).reversed())
-        .limit(3)
-        .collect(Collectors.toList());
-  }
-
-  public double calcCurrentYearCheapBooksTotalValue() {
-    return products.stream()
-        .filter(p -> "Book".equalsIgnoreCase(p.getType()) && p.getPrice() <= 75)
-        .filter(p -> p.getAdditionDate().getYear() == LocalDateTime.now().getYear())
-        .mapToDouble(Product::getPrice).sum();
-  }
-
-  public Map<String, List<Product>> groupByType() {
-    return products.stream()
-        .collect(Collectors.groupingBy(Product::getType));
-  }
-
-  public static String output(List<Product> products) {
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < products.size(); i++) {
-      sb.append("Product#");
-      sb.append(i + 1);
-      sb.append(":\n");
-      sb.append(products.get(i));
+    /**
+     * Gets Product instances, which type is "book" and price is greater than 250
+     *
+     * @return List<Product>
+     */
+    public List<Product> getExpansiveBooks() {
+        return products.stream()
+                .filter(p -> p.getPrice() > 250 && "Book".equalsIgnoreCase(p.getType()))
+                .collect(Collectors.toList());
     }
-    return sb.toString();
-  }
+
+    /**
+     * Gets Product instances, which type is "book" and they are discountable.
+     * Makes discount 10% and returns list after that.
+     *
+     * @return List<Product>
+     */
+    public List<Product> applyDiscountToBooks() {
+        return products.stream()
+                .filter(p -> p.isDiscountable() && "Book".equalsIgnoreCase(p.getType()))
+                .peek(p -> p.setPrice(p.getPrice() - p.getPrice() * 0.1))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Gets Product instances, which type is "book" and price is minimal
+     *
+     * @return Product - an instance of the cheapest product.
+     * @throws ProductNotFoundException when list of Product instances is empty.
+     */
+    public Product getTheCheapestBook() throws ProductNotFoundException {
+        if (products.stream().noneMatch(p -> "Book".equalsIgnoreCase(p.getType()))) {
+            throw new ProductNotFoundException("Book");
+        }
+        return products.stream()
+                .filter(p -> "Book".equalsIgnoreCase(p.getType()))
+                .min(Comparator.comparing(Product::getPrice))
+                .orElseGet(() -> new Product("000-000-000", "", 0.00));
+    }
+
+    /**
+     * Gets three the latest added Product instances
+     *
+     * @return List<Product>
+     */
+    public List<Product> getTheLatestAddedProducts() {
+        return products.stream()
+                .sorted(Comparator.comparing(Product::getAdditionDate).reversed())
+                .limit(3)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Calculates total price of Product instances, which type is "book" and price is non-greater than 75.
+     *
+     * @return double
+     */
+    public double calcCurrentYearCheapBooksTotalValue() {
+        return products.stream()
+                .filter(p -> "Book".equalsIgnoreCase(p.getType()) && p.getPrice() <= 75)
+                .filter(p -> p.getAdditionDate().getYear() == LocalDateTime.now().getYear())
+                .mapToDouble(Product::getPrice).sum();
+    }
+
+    /**
+     * Groups Product instances by type. Returns map, which key is type and value is list of Product instances.
+     *
+     * @return Map<String, List < Product>>
+     */
+    public Map<String, List<Product>> groupByType() {
+        return products.stream()
+                .collect(Collectors.groupingBy(Product::getType));
+    }
+
+    /**
+     * Static method for output list of Product instances as String.
+     *
+     * @param products list of Product instances
+     * @return String
+     */
+    public static String output(List<Product> products) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < products.size(); i++) {
+            sb.append("Product#");
+            sb.append(i + 1);
+            sb.append(":\n");
+            sb.append(products.get(i));
+        }
+        return sb.toString();
+    }
 }
